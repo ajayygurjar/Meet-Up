1132ac70d83b6f915eeee377490700b79b9c358b
ajayygurjar <167853061+ajayygurjar@users.noreply.github.com>
2025-06-20 10:08:33 +0530

    Dynamic routing

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,15 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+
+const MeetingDetails = () => {
+
+	return <>
+	<MeetupDetail image="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg/1024px-View_of_Empire_State_Building_from_Rockefeller_Center_New_York_City_dllu_%28cropped%29.jpg"
+	 title='First Meetup'
+	  address='Some Street 5,Some City'
+	   description='This is a first meetup'
+	    />
+	</>
+};
+
+export default MeetingDetails;
