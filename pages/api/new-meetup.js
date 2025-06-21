dd02768eaa7d79cbee4a7e0aa5e8a289c645cf3e
ajayygurjar <167853061+ajayygurjar@users.noreply.github.com>
2025-06-21 19:54:24 +0530

    Posting meetups into mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -4,14 +4,20 @@ import { MongoClient } from "mongodb";
 async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    
-    const client= await MongoClient.connect('mongodb+srv://ajay1651:ajay1651@cluster0.sg2ydfa.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0')
+    try {
+    const client= await MongoClient.connect('mongodb+srv://ajay1651:acsbjwK3KcNte8li@cluster0.sg2ydfa.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0')
     const db=client.db();
   const meetupsCollection=db.collection('meetups');
    const result =await meetupsCollection.insertOne(data);
    console.log(result);
    client.close();
    res.status(201).json({message:'Meetup inserted!'});
+  }catch (error) {
+      console.error("API Error:", error); // This will print the real issue
+      res.status(500).json({ message: "Internal server error" });
+    }
+  } else {
+    res.status(405).json({ message: "Method Not Allowed" });
   }
 
 }
